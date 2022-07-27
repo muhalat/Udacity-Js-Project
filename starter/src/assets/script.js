cc36ee00222585545db94ee414eb0bc80cdc1be3
Muhalat <muhalat17@gmail.com>
2022-07-27 19:28:01 +0100

    added the products using object literals

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,4 +1,4 @@
-/* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
+
 
 /* Create 3 or more product objects using object literal notation 
    Each product should include five properties
@@ -8,6 +8,29 @@
    - productId: unique id for the product (number)
    - image: picture of product (url string)
 */
+const productOne = {
+  name: "Carton of Cherries", 
+  price: 4,
+  quantity: 0,
+  productId: 100,
+  image: 'images/cherry.jpg'
+};
+const productTwo = {
+  name: "Carton of Strawberries", 
+  price: 5,
+  quantity: 0,
+  productId: 101,
+  image: 'images/strawberry.jpg'
+};
+const productThree = {
+  name: "Bag of Oranges", 
+  price: 10,
+  quantity: 0,
+  productId: 103,
+  image: 'images/orange.jpg'
+};
+/* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
+const products = [productOne,productTwo,productThree]
 
 /* Images provided in /images folder. All images from Unsplash.com
    - cherry.jpg by Mae Mu
@@ -16,30 +39,45 @@
 */
 
 /* Declare an empty array named cart to hold the items in the cart */
-
+const cart = []
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
   - addProductToCart should then increase the product's quantity
   - if the product is not already in the cart, add it to the cart
 */
-
+function addProductToCart(productId){
+ products.quantity+=1;
+ cart.push(products)
+ }
+addProductToCart(100);
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
-
+function increaseQuantity(_productId){
+  quantity+=1
+}
+increaseQuantity(100);
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
   - decreaseQuantity should decrease the quantity of the product
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
-
+  function decreaseQuantity(_productId){
+    quantity-=1
+    if (quantity==0){
+      cart.pop()
+    }
+  }
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
   - removeProductFromCart should remove the product from the cart
 */
-
+// function removeProductFromCart(productId){
+//   quantity = 0
+//   cart.pop()
+// }
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total of all products
   - cartTotal should return the sum of the products in the cart
